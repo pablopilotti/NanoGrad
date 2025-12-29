dac6aa95184d4289d713624bb8ebac815fb22056
Pablo Pilotti <ppilotti@irobot.com>
2025-12-29 12:16:07 +0000

    docs: add exercises and evaluation results to MakeMore.py

--- MakeMore/MakeMore.py
+++ MakeMore/MakeMore.py
@@ -742,3 +742,34 @@ def main():
 
 if __name__ == "__main__":
     main()
+
+# Exercises:
+# E01: train a trigram language model, i.e. take two characters as an input to predict the 3rd one. 
+#      Feel free to use either counting or a neural net. Evaluate the loss; Did it improve over a bigram model?
+# E02: split up the dataset randomly into 80% train set, 10% dev set, 10% test set. 
+#      Train the bigram and trigram models only on the training set. Evaluate them on dev and test splits. What can you see?
+#
+# $ python3 MakeMore.py --all --evaluate ./all-names --file names_test.txt
+# Evaluating all model(s) against names_test.txt..
+# Bigram table model loss: 2.4534
+# Trigram table model loss: 2.0934
+# Bigram neural net model loss: 2.4822
+# Trigram neural net model loss: 2.2648
+
+# $ python3 MakeMore.py --all --evaluate ./all-names --file names_dev.txt
+# Evaluating all model(s) against names_dev.txt..
+# Bigram table model loss: 2.4534
+# Trigram table model loss: 2.0934
+# Bigram neural net model loss: 2.4602
+# Trigram neural net model loss: 2.2458
+
+# E03: use the dev set to tune the strength of smoothing (or regularization) for the trigram model - 
+#       i.e. try many possibilities and see which one works best based on the dev set loss. 
+#       What patterns can you see in the train and dev set loss as you tune this strength? 
+#       Take the best setting of the smoothing and evaluate on the test set once and at the end. 
+#       How good of a loss do you achieve?
+# E04: we saw that our 1-hot vectors merely select a row of W, so producing these vectors explicitly feels wasteful. 
+#      Can you delete our use of F.one_hot in favor of simply indexing into rows of W?
+# E05: look up and use F.cross_entropy instead. You should achieve the same result. 
+#       Can you think of why we'd prefer to use F.cross_entropy instead?
+# E06: meta-exercise! Think of a fun/interesting exercise and complete it.
